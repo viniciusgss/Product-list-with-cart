eb964fe96a726f1832047f8ac9d9eca2a474dd95
viniciusgss <viniciusgss22@gmail.com>
2024-10-10 09:32:46 -0300

    com imagem e com carrinho feito

--- src/entines/cart.ts
+++ src/entines/cart.ts
@@ -29,7 +29,49 @@ export class Cart {
     }
 
     this.calculateTotal();
-    console.log(Cart._products);
+
+    // Atualiza o carrinho de compras no HTML
+    this.toHTML();
+    // console.log(Cart._products);
+  }
+
+  static toHTML() {
+    const cartContainerHTML = document.getElementById("cart-container");
+
+    if (!cartContainerHTML) return;
+
+    const totalQuantityHTML = cartContainerHTML.querySelector(
+      "#total-quantity-text"
+    );
+
+    if (!totalQuantityHTML) return;
+    totalQuantityHTML.textContent = this._totalQuantity.toString();
+
+    let ulProductsHTML = cartContainerHTML.querySelector("ul");
+
+    if (ulProductsHTML) {
+      ulProductsHTML.innerHTML = "";
+    } else {
+      ulProductsHTML = document.createElement("ul");
+    }
+
+    for (const product of this._products) {
+      const liProductHTML = document.createElement("li");
+
+      const productHTML = `
+        <span>${product.name}</span>
+        <div>
+          <span>${product.quantity}x</span>
+          <span>@$${product.price}</span>
+          <span>$${product.total}</span>
+        </div>
+      `;
+
+      liProductHTML.innerHTML = productHTML;
+      ulProductsHTML.appendChild(liProductHTML);
+    }
+
+    cartContainerHTML.appendChild(ulProductsHTML);
   }
 
   static get products() {
--- src/entines/product.ts
+++ src/entines/product.ts
@@ -4,7 +4,7 @@ import { Cart } from "./cart";
 export class Product {
   private _id: string = uuidv4();
   private _name: string;
-  private _category: string;  
+  private _category: string;
   private _price: number;
   private _imageUrl: string;
   private _quantity: number = 0;
@@ -17,14 +17,43 @@ export class Product {
     this._imageUrl = imageUrl;
   }
 
-toHTML(){
-    const productListHtml = document.getElementById("products-list")
-    const productHtml = document.createElement("li")
-    
-}
+  toHTML() {
+    const productListHTML = document.getElementById("product-list");
+
+    if (!productListHTML) return;
+
+    const productHTML = document.createElement("li");
+    productHTML.id = this._id;
+
+    productHTML.innerHTML = `
+        <div class="rounded-xl overflow-hidden flex flex-col border border-black h-[270px] w-[250px]">
+          <div class="mb-10 relative h-full">
+            <img src="${this._imageUrl}" alt="${this._name}" class="object-cover w-full h-full" />
+            <button id="button-add-to-cart-${this._id}" type="button" class="button rounded-lg text-white font-medium text-xs bg-red-400">Add to Cart</button>
+          </div>
+
+          <div class="flex flex-col p-2">
+            <span class="product-category">${this._category}</span>
+            <span class="product-name text-xl font-medium">${this._name}</span>
+            <span class="product-price">$${this._price.toFixed(2)}</span>
+          </div>
+        </div>
+    `;
+
+    const buttonAddToCartHTML = productHTML.querySelector(
+      `#button-add-to-cart-${this._id}`
+    );
+    buttonAddToCartHTML?.addEventListener("click", () => this.incrementQuantity());
+
+    productListHTML.appendChild(productHTML);
+  }
 
   get id() {
     return this._id;
+  }
+
+  get name() {
+    return this._name;
   }
 
   get quantity() {
@@ -35,21 +64,25 @@ toHTML(){
     return this._total;
   }
 
-  calculateTotal() {
+  private calculateTotal() {
     this._total = this._quantity * this._price;
   }
 
   incrementQuantity() {
     this._quantity++;
     this.calculateTotal();
-
     Cart.addToCart(this);
   }
 
   decrementQuantity() {
-    this._quantity--;
-    this.calculateTotal();
-    // TODO: remover o produto do carrinho se a quantidade for 0
+    if (this._quantity > 0) {
+      this._quantity--;
+      this.calculateTotal();
+      // Remover do carrinho se a quantidade for 0
+      if (this._quantity === 0) {
+        Cart.removeProduct(this);
+      }
+    }
   }
 
   get price() {
